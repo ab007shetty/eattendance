b8555b80732b83b1072f62c5ece068084d8a3745
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2021-06-18 21:46:06 +0530

    Added Scrol to Top Button

--- public/assets/js/header_index.js
+++ public/assets/js/header_index.js
@@ -71,5 +71,20 @@ $(document).scroll(function() {
       })
 
 
+$(document).ready(function() {
+$(window).scroll(function() {
+if ($(this).scrollTop() > 20) {
+$('#toTopBtn').fadeIn();
+} else {
+$('#toTopBtn').fadeOut();
+}
+});
 
+$('#toTopBtn').click(function() {
+$("html, body").animate({
+scrollTop: 0
+}, 1000);
+return false;
+});
+});
 
